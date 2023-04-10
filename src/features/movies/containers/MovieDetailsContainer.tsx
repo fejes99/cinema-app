fb425d286d6d1bea55258f5576701e439aff19a4
David Fejes <35314157+fejes99@users.noreply.github.com>
2023-04-10 15:26:13 +0200

    Implemented movie filters

---
+++ src/features/movies/containers/MovieDetailsContainer.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+interface Props {}
+
+const MovieDetailsContainer: React.FC<Props> = () => {
+  return <div>MovieDetailsContainer</div>;
+};
+
+export default connect()(MovieDetailsContainer);
