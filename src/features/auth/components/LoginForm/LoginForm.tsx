247a9c86d97057edc12f6e0cdd8e87d2fa5187f9
David Fejes <35314157+fejes99@users.noreply.github.com>
2023-06-01 11:37:21 +0200

    Added form validation

--- src/features/auth/components/LoginForm/LoginForm.tsx
+++ src/features/auth/components/LoginForm/LoginForm.tsx
@@ -16,20 +16,62 @@ const LoginForm: React.FC<Props> = ({ onSubmit }) => {
     password: '',
   });
 
-  const isFormValid = Object.values(login).every((value) => value !== '');
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+  });
+
+  const isFormValid = login.email.trim() !== '' && login.password.trim() !== '';
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = event.target;
     setLogin((prevState) => ({ ...prevState, [name]: value }));
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: '' }));
   };
 
-  const handleSubmit = (): void => onSubmit(login);
+  const handleSubmit = (): void => {
+    const validationErrors = validateForm();
+    if (Object.values(validationErrors).some((value) => value !== '')) {
+      setErrors(validationErrors);
+    } else {
+      setErrors({
+        email: '',
+        password: '',
+      });
+      onSubmit(login);
+    }
+  };
+
+  const validateForm = (): { email: string; password: string } => {
+    const validationErrors: { email: string; password: string } = {
+      email: '',
+      password: '',
+    };
+
+    if (!login.email.trim()) {
+      validationErrors.email = 'Email is required';
+    } else if (!isValidEmail(login.email)) {
+      validationErrors.email = 'Invalid email address';
+    }
+
+    if (!login.password.trim()) {
+      validationErrors.password = 'Password is required';
+    }
+
+    return validationErrors;
+  };
+
+  const isValidEmail = (email: string): boolean => {
+    // Email validation regex
+    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    return emailRegex.test(email);
+  };
 
   return (
     <div className='login'>
       <div className='login__title'>Login</div>
       <div className='login__form'>
-        <div className='login__field'>
+        <div className={`login__field ${errors.email ? 'error' : ''}`}>
           <Input
             label='Email'
             type='text'
@@ -37,9 +79,11 @@ const LoginForm: React.FC<Props> = ({ onSubmit }) => {
             value={login.email}
             onChange={handleChange}
           />
+          {errors.email && <div className='error-message'>{errors.email}</div>}
         </div>
-        <div className='login__field'>
+        <div className={`login__field ${errors.password ? 'error' : ''}`}>
           <InputPassword confirmPassword={false} value={login.password} onChange={handleChange} />
+          {errors.password && <div className='error-message'>{errors.password}</div>}
         </div>
         <Button size='medium' type='success' disabled={!isFormValid} onClick={handleSubmit}>
           Login
--- src/features/auth/components/RegisterForm/RegisterForm.tsx
+++ src/features/auth/components/RegisterForm/RegisterForm.tsx
@@ -20,18 +20,114 @@ const RegisterForm: React.FC<Props> = ({ onSubmit }) => {
     password: '',
   });
 
-  const isFormValid = Object.values(register).every((value) => value !== '');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const [errors, setErrors] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    username: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const isFormValid =
+    register.firstName.trim() !== '' &&
+    register.lastName.trim() !== '' &&
+    register.email.trim() !== '' &&
+    register.username.trim() !== '' &&
+    register.password.trim() !== '' &&
+    register.password === confirmPassword;
 
   const { redirectToLogin } = useAuthRedirect();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     const { name, value } = event.target;
     setRegister((prevState) => ({ ...prevState, [name]: value }));
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: '' }));
+  };
+
+  const handleConfirmPasswordChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const { value } = event.target;
+    setConfirmPassword(value);
+    setErrors((prevErrors) => ({ ...prevErrors, confirmPassword: '' }));
   };
 
   const handleSubmit = (): void => {
-    onSubmit(register);
-    redirectToLogin();
+    const validationErrors = validateForm();
+    if (Object.values(validationErrors).some((value) => value !== '')) {
+      setErrors(validationErrors);
+    } else {
+      setErrors({
+        firstName: '',
+        lastName: '',
+        email: '',
+        username: '',
+        password: '',
+        confirmPassword: '',
+      });
+      onSubmit(register);
+      redirectToLogin();
+    }
+  };
+
+  const validateForm = (): {
+    firstName: string;
+    lastName: string;
+    email: string;
+    username: string;
+    password: string;
+    confirmPassword: string;
+  } => {
+    const validationErrors: {
+      firstName: string;
+      lastName: string;
+      email: string;
+      username: string;
+      password: string;
+      confirmPassword: string;
+    } = {
+      firstName: '',
+      lastName: '',
+      email: '',
+      username: '',
+      password: '',
+      confirmPassword: '',
+    };
+
+    if (!register.firstName.trim()) {
+      validationErrors.firstName = 'First Name is required';
+    }
+
+    if (!register.lastName.trim()) {
+      validationErrors.lastName = 'Last Name is required';
+    }
+
+    if (!register.email.trim()) {
+      validationErrors.email = 'Email is required';
+    } else if (!isValidEmail(register.email)) {
+      validationErrors.email = 'Invalid email address';
+    }
+
+    if (!register.username.trim()) {
+      validationErrors.username = 'Username is required';
+    }
+
+    if (!register.password.trim()) {
+      validationErrors.password = 'Password is required';
+    }
+
+    if (register.password !== confirmPassword) {
+      validationErrors.confirmPassword = 'Passwords do not match';
+    }
+
+    return validationErrors;
+  };
+
+  const isValidEmail = (email: string): boolean => {
+    // Email validation regex
+    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    return emailRegex.test(email);
   };
 
   return (
@@ -39,7 +135,7 @@ const RegisterForm: React.FC<Props> = ({ onSubmit }) => {
       <div className='register__title'>Register</div>
       <div className='register__form'>
         <div className='register__name-wrapper'>
-          <div className='register__field register__field-name'>
+          <div className={`register__field ${errors.firstName ? 'error' : ''}`}>
             <Input
               label='First Name'
               type='text'
@@ -47,8 +143,9 @@ const RegisterForm: React.FC<Props> = ({ onSubmit }) => {
               value={register.firstName}
               onChange={handleChange}
             />
+            {errors.firstName && <div className='error-message'>{errors.firstName}</div>}
           </div>
-          <div className='register__field register__field-name'>
+          <div className={`register__field ${errors.lastName ? 'error' : ''}`}>
             <Input
               label='Last Name'
               type='text'
@@ -56,9 +153,10 @@ const RegisterForm: React.FC<Props> = ({ onSubmit }) => {
               value={register.lastName}
               onChange={handleChange}
             />
+            {errors.lastName && <div className='error-message'>{errors.lastName}</div>}
           </div>
         </div>
-        <div className='register__field'>
+        <div className={`register__field ${errors.email ? 'error' : ''}`}>
           <Input
             label='Email'
             type='text'
@@ -66,8 +164,9 @@ const RegisterForm: React.FC<Props> = ({ onSubmit }) => {
             value={register.email}
             onChange={handleChange}
           />
+          {errors.email && <div className='error-message'>{errors.email}</div>}
         </div>
-        <div className='register__field'>
+        <div className={`register__field ${errors.username ? 'error' : ''}`}>
           <Input
             label='Username'
             type='text'
@@ -75,17 +174,24 @@ const RegisterForm: React.FC<Props> = ({ onSubmit }) => {
             value={register.username}
             onChange={handleChange}
           />
+          {errors.username && <div className='error-message'>{errors.username}</div>}
         </div>
-        <div className='register__field'>
+        <div className={`register__field ${errors.password ? 'error' : ''}`}>
           <InputPassword
             confirmPassword={false}
             value={register.password}
             onChange={handleChange}
           />
+          {errors.password && <div className='error-message'>{errors.password}</div>}
         </div>
-        {/* <div className='register__field'>
-          <InputPassword confirmPassword={true} value={confirmPassword} onChange={handleChange} />
-        </div> */}
+        <div className={`register__field ${errors.confirmPassword ? 'error' : ''}`}>
+          <InputPassword
+            confirmPassword={true}
+            value={confirmPassword}
+            onChange={handleConfirmPasswordChange}
+          />
+          {errors.confirmPassword && <div className='error-message'>{errors.confirmPassword}</div>}
+        </div>
         <Button size='medium' type='success' disabled={!isFormValid} onClick={handleSubmit}>
           Register
         </Button>
