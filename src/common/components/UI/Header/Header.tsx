80c6e11da5f888511e6d39ff8f6911f009a4545f
David Fejes <35314157+fejes99@users.noreply.github.com>
2023-06-26 12:25:06 +0200

    Added type to component

--- src/common/components/UI/Header/Header.tsx
+++ src/common/components/UI/Header/Header.tsx
@@ -3,7 +3,7 @@ import './Header.scss';
 import Logo from './Logo/Logo';
 import Navbar from './Navbar/Navbar';
 
-const Header = () => (
+const Header: React.FC = () => (
   <header className='header'>
     <div className='header__container'>
       <Logo />
